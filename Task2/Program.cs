e21a77a7b8b32e28805272912a195c0aa89483d0
Isaac Chukwuemeke Oselukwue <princeizak@live.com>
2020-09-13 20:23:06 +0100

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public int GetHighestValue(List<MyTypes> list)
+        {
+            return list.Count > 0 ? list.Max(t => t.Age) : 0; //could also return -1
+        }
+        public void maxvalue()
+        {
+            List<int> primeNumbers = new List<int>();
+
+            string elements = Console.ReadLine();
+        }
+    }
+}
