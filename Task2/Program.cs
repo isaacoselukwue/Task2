e81d1ae27f21dbb53d49a9417465a6b778d7948a
Isaac Chukwuemeke Oselukwue <princeizak@live.com>
2020-09-13 21:10:24 +0100

    a function that returns the largest element in a list done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -11,16 +11,25 @@ namespace Task2
         static void Main(string[] args)
         {
 
+            Console.ReadLine();
         }
-        public int GetHighestValue(List<MyTypes> list)
-        {
-            return list.Count > 0 ? list.Max(t => t.Age) : 0; //could also return -1
-        }
+        //public int GetHighestValue(List<MyTypes> list)
+        //{
+        //    return list.Count > 0 ? list.Max(t => t.Age) : 0; //could also return -1
+        //}
         public void maxvalue()
         {
-            List<int> primeNumbers = new List<int>();
-
-            string elements = Console.ReadLine();
+            //a function that returns the largest element in a list.
+            List<int> numSet = new List<int>();
+            Console.WriteLine("Enter No of Elements");
+            int sizeOfList = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i< sizeOfList; i++)
+            {
+                Console.WriteLine("Please enter a number");
+                numSet.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            var max = numSet.Max();
+            Console.WriteLine($"The Largest Element in List is: {max}");
         }
     }
 }
