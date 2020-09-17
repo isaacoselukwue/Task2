61ee4c8a44c80caaf6e512cfdce734dae131c1d6
Isaac Chukwuemeke Oselukwue <princeizak@live.com>
2020-09-17 17:26:54 +0100

    Included num to word and started working on tying up the caller method

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -10,6 +10,58 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+            //Console.Write("Guess a number between 1 and 10: ");
+            //int guessed = 0;
+            //Random myRandom = new Random();
+            //int num = myRandom.Next(1, 10);
+            //while (guessed != num)
+            //{
+            //    Console.WriteLine("Guess the number: ");
+            //    int input = Convert.ToInt32(Console.ReadLine());
+            //    if (num < input)
+            //    {
+            //        Console.WriteLine("too high");
+            //    }
+            //    else if (num > input)
+            //    {
+            //        Console.WriteLine("too low");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("correct");
+            //        break;
+            //    }
+            //}
+
+            //int number = 0;
+            //Console.WriteLine("Enter a number");
+            //try
+            //{
+            //    number = Convert.ToInt32(Console.ReadLine());
+            //    if (number == 0)
+            //    {
+            //        Console.WriteLine("Zero");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Num is: {numberToWord(number)}");
+            //    }
+
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("Enter an integer: ");
+            //    throw;
+            //}
+            string input = Console.ReadLine();
+            bool run = true;
+            while (run)
+            {
+                if (input == "1")
+                {
+                    maxvalue();
+                }
+            }
 
             Console.ReadLine();
         }
@@ -17,7 +69,7 @@ namespace Task2
         //{
         //    return list.Count > 0 ? list.Max(t => t.Age) : 0; //could also return -1
         //}
-        public void maxvalue()
+        public static void maxvalue()
         {
             //a function that returns the largest element in a list.
             List<int> numSet = new List<int>();
@@ -153,6 +205,60 @@ namespace Task2
             int totalSum = numSet.Sum();
             Console.WriteLine($"The sum of the numbers in the List is: {totalSum}");
             return numSum;
+        }
+        public static string numberToWord(int number)
+        {
+            string words = "";
+            List<string> unitList = new List<string> { "zero", "one", "two", "three", "four", "five", "six",
+                      "seven", "eight", "nine", "ten", "eleven", "twelve",
+                      "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+                      "eighteen", "nineteen"};
+            List<string> tensList = new List<string> { "zero", "ten", "twenty", "thirty", "forty", "fifty",
+                      "sixty", "seventy", "eighty", "ninety" };
+
+            if (number == 0)
+            {
+                return "zero";
+            }
+
+            if (number < 0)
+            {
+                String numberStr = "" + number; 
+                numberStr = numberStr.Substring(1);
+                return "minus " + numberToWord(Convert.ToInt32(numberStr));
+            }
+            if ((number / 1000000) > 0)
+            {
+                words += numberToWord(number / 1000000) + " million ";
+                number %= 1000000;
+            }
+            if ((number / 1000) > 0)
+            {
+                words += numberToWord(number / 1000) + " thousand ";
+                number %= 1000;
+            }
+            if ((number / 100) > 0)
+            {
+                words += numberToWord(number / 100) + " hundred and ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (number < 20)
+                {
+                    words += unitList[number];
+                }
+                else
+                {
+                    words += tensList[number / 10];
+                    if ((number % 10) > 0)
+                    {
+                        words += "-" + unitList[number % 10];
+                    }
+                }
+            }
+            return words;
         }
     }
 }
--- Task2/Task3.cs
+++ Task2/Task3.cs
@@ -10,22 +10,117 @@ namespace Task2
     {
         //random no game
         public string MyProperty(string random) {
-            Console.Write("Enter a number btw 1 and 1000: ");
-            int guessed = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Guess a number between 1 and 10: ");
+            int guessed = 0;
             Random myRandom = new Random();
-            int num = myRandom.Next(1, 1000);
+            int num = myRandom.Next(1, 10);
             while (guessed != num)
             {
-                if (num > guessed)
+                Console.WriteLine("Guess the number: ");
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (num < input)
                 {
                     Console.WriteLine("too high");
                 }
-                else
+                else if (num > input)
+                {
                     Console.WriteLine("too low");
-                Console.Write("Enter a number btw 1 and 1000: ");
-                //break;
+                }
+                else
+                {
+                    Console.WriteLine("correct");
+                    break;
+                }
             }
             return random;
+        }
+    }
+    class Task4
+    {
+        public string wordnos(string num) {
+            int number = 0;
+            Console.WriteLine("Enter a number");
+            try
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number == 0)
+                {
+                    Console.WriteLine("Zero");
+                }
+                else
+                {
+                    Console.WriteLine($"Num is: {numberToWord(number)}");
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Enter an integer: ");
+                throw;
+            }
+            return num;
+        }
+        public string numberToWord(int number) {
+            string words = "";
+            List<string> unitList = new List<string> { "zero", "one", "two", "three", "four", "five", "six",
+                      "seven", "eight", "nine", "ten", "eleven", "twelve",
+                      "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+                      "eighteen", "nineteen"};
+            List<string> tensList = new List<string> { "zero", "ten", "twenty", "thirty", "forty", "fifty",
+                      "sixty", "seventy", "eighty", "ninety" };
+
+            if (number == 0)
+            {
+                return "zero";
+            }
+            
+            if (number < 0)
+            {
+                // convert the number to a string
+                String numberStr = "" + number;
+                // remove minus before the number 
+                numberStr = numberStr.Substring(1);
+                // add minus before the number and convert the rest of number 
+                return "minus " + numberToWord(Convert.ToInt32(numberStr));
+            }
+            // check if number is divisible by 1 million
+            if ((number / 1000000) > 0)
+            {
+                words += numberToWord(number / 1000000) + " million ";
+                number %= 1000000;
+            }
+            // check if number is divisible by 1 thousand
+            if ((number / 1000) > 0)
+            {
+                words += numberToWord(number / 1000) + " thousand ";
+                number %= 1000;
+            }
+            // check if number is divisible by 1 hundred
+            if ((number / 100) > 0)
+            {
+                words += numberToWord(number / 100) + " hundred and ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                // check if number is within teens
+                if (number < 20)
+                {
+                    // fetch the appropriate value from unit array
+                    words += unitList[number];
+                }
+                else
+                {
+                    // fetch the appropriate value from tens array
+                    words += tensList[number / 10];
+                    if ((number % 10) > 0)
+                    {
+                        words += "-" + unitList[number % 10];
+                    }
+                }
+            }
+            return words;
         }
     }
 }
